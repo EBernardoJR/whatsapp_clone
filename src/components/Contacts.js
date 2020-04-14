c992540fa326600fa9961e5dd8369cf5c192ccbd
EbernardoJR <emanoelbernardocontato@outlook.com>
2020-04-14 19:22:22 -0300

    finished

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,25 +1,48 @@
-import React, { useEffect } from 'react'
-import { View, Text } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { View, Text, FlatList } from 'react-native'
 import { connect } from 'react-redux'
 import { userContactsFetch } from '../actions/appActions'
+import _ from 'lodash'
 
 function Contacts(props){
 
+    const [ _contacts, setContacts ] = useState([])
+
     useEffect(()=>{
-        props.userContactsFetch()
+        props.userContactsFetch()//buscar os contatos para pôr no estado do redux
+        makeDataSource(props.contacts)
     }, [])
 
+
+
+    //fonte de dados para o listView
+
+    //criando a fonte de dados
+    function makeDataSource(contacts){
+        setContacts(contacts)
+    }
+
+
     return(
-        <View>
-            <Text>Contatos</Text>
-        </View>
+        <FlatList 
+        data={_contacts}
+        renderItem={({data}) => <View><Text>{data}</Text></View>}
+        keyExtractor={item => item.uid}
+        />
     )
 }
 
-const mapStateToProps = state => (
-    {
+const mapStateToProps = state => {
+    //tranformando o objeto em um  array
+    const contacts = _.map(state.contactListReducer, (value, uid) => {
+        return { ...value, uid }
+    })
+ 
 
+    return {
+        contacts,
+        addContactSucess: state.appReducer.addContactSucess //quando for adicionado um novo contato vai forçar a atualização
     }
-)
+}
 
-export default connect(null, { userContactsFetch })(Contacts)
+export default connect(mapStateToProps, { userContactsFetch })(Contacts)
--- src/reducers/contactListReducer.js
+++ src/reducers/contactListReducer.js
@@ -1,3 +1,7 @@
+import { CONTACT_LIST_USER } from '../actions/type'
+
+
+
 const INITIAL_STATE =  {
 
 }
@@ -5,6 +9,8 @@ const INITIAL_STATE =  {
 export default (state = INITIAL_STATE, action) => {
     switch(action.type){
 
+        case CONTACT_LIST_USER:
+            return action.payload
 
         default: 
             return state
